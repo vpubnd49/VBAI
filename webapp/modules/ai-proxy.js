9cedb9472f9f64221a1eac062eb540af3627c306
vpubnd49 <haichau2404@gmail.com>
2026-09-17 22:09:55 +0700

    fix: tang timeout boc bang am thanh tu 2 phut len 5 phut

--- webapp/modules/ai-proxy.js
+++ webapp/modules/ai-proxy.js
@@ -440,7 +440,7 @@ async function sendSingleAudioTranscription(file, model = '', options = {}, part
   const response = await backendFetch('/transcribe', {
     method: 'POST',
     body: formData,
-    timeoutMs: options.timeoutMs ?? 180000,
+    timeoutMs: options.timeoutMs ?? 300000,
   });
 
   if (!response.ok) {
